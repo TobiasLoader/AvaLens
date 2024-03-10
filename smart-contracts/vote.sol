57613bb5c7c9efd90a4a0dad61b6aba0b2a786df
yunokiisshin <yunokiisshin@gmail.com>
2024-03-10 15:51:54 +0000

    working vote sc

--- smart-contracts/vote.sol
+++ smart-contracts/vote.sol
@@ -3,144 +3,122 @@
 
 // SPDX-License-Identifier: Ecosystem
 
-// modified the code from Avalanche Academy Teleporter course
+// from Avalanche Academy
 
 pragma solidity 0.8.18;
 
-
 import "https://github.com/ava-labs/teleporter/blob/main/contracts/src/Teleporter/ITeleporterMessenger.sol";
-import "https://github.com/ava-labs/teleporter/blob/main/contracts/src/Teleporter/ITeleporterReceiver.sol";
 
-/**
- * @dev ExampleCrossChainMessenger is an example contract that demonstrates how to send and receive
- * messages cross chain.
- */
 contract Vote {
 
-    ITeleporterMessenger public immutable teleporterMessenger = ITeleporterMessenger(0x253b2784c75e510dD0fF1da844684a1aC0aa5fcf)
+    ITeleporterMessenger public immutable teleporterMessenger = ITeleporterMessenger(0x253b2784c75e510dD0fF1da844684a1aC0aa5fcf);
 
-    // Mapping of imageId to votes received
-    mapping(bytes32 => uint256) public userVotes;
-
-    // Mapping of imageId to the publicId of the photographer (imgOwner)
-    mapping(bytes32 => bytes32) public imageOwners;
-
-    // Mapping of imgOwner to their total merits
-    mapping(bytes32 => uint256) public ownerMerits;
-
-    constructor() {
-        // Initialize userVotes mapping within the constructor
-        userVotes[bytes32("Image1")] = 91;
-        userVotes[bytes32("Image2")] = 72;
-        userVotes[bytes32("Image3")] = 77;
-        userVotes[bytes32("Image4")] = 59;
-        userVotes[bytes32("Image5")] = 36;
-        userVotes[bytes32("Image6")] = 30;
-        userVotes[bytes32("Image7")] = 26;
-        userVotes[bytes32("Image8")] = 22;
-        userVotes[bytes32("Image9")] = 12;
-        userVotes[bytes32("Image10")] = 10;
-        
-
-        // Initialize imageOwners mapping
-        imageOwners[bytes32("Image1")] = bytes32("De0vYJ7k7D");
-        imageOwners[bytes32("Image2")] = bytes32("qkGzdCRY9e");
-        imageOwners[bytes32("Image3")] = bytes32("KM7HRQydNZ");
-        imageOwners[bytes32("Image4")] = bytes32("eipbLjnyeG");
-        imageOwners[bytes32("Image5")] = bytes32("gyGE5kaecT");
-        imageOwners[bytes32("Image6")] = bytes32("76VfzkXOtG");
-        imageOwners[bytes32("Image7")] = bytes32("N6SrxMNX8w");
-        imageOwners[bytes32("Image8")] = bytes32("JFktwjtSka");
-        imageOwners[bytes32("Image9")] = bytes32("DXM951ZMtd");
-        imageOwners[bytes32("Image10")] = bytes32("sf6Qd9Vk2Y");
-
-        // Initialize ownerMerits mapping
-        ownerMerits[bytes32("De0vYJ7k7D")] = 0;
-        ownerMerits[bytes32("qkGzdCRY9e")] = 0;
-        ownerMerits[bytes32("KM7HRQydNZ")] = 0;
-        ownerMerits[bytes32("eipbLjnyeG")] = 0;
-        ownerMerits[bytes32("gyGE5kaecT")] = 0;
-        ownerMerits[bytes32("76VfzkXOtG")] = 0;
-        ownerMerits[bytes32("N6SrxMNX8w")] = 0;
-        ownerMerits[bytes32("JFktwjtSka")] = 0;
-        ownerMerits[bytes32("DXM951ZMtd")] = 0;
-        ownerMerits[bytes32("sf6Qd9Vk2Y")] = 0;
-    }
-
-    constructor(address teleporterMessengerAddress) {
-        teleporterMessenger = ITeleporterMessenger(teleporterMessengerAddress);
-    }
-
-
-    // Function to calculate and assign merits based on votes
-    function calculateMerit(bytes32[] calldata imageIds) external {
-        
-        ownerMerits[bytes32("De0vYJ7k7D")] = 10; // 1st
-        ownerMerits[bytes32("qkGzdCRY9e")] = 5;  // 2nd
-        ownerMerits[bytes32("KM7HRQydNZ")] = 2;  // ...
-        ownerMerits[bytes32("eipbLjnyeG")] = 1;
-        ownerMerits[bytes32("gyGE5kaecT")] = 1;
-    }
-
-    /**
-     * @dev Convert votes to merits.
-     * @param votes The number of votes to convert.
-     * @return merits The corresponding merits based on the votes.
-     */
-    function votesToMerits(uint256 votes) public pure returns (uint256 merits) {
-        // Define your conversion logic here. For simplicity, this example assumes 1 vote = 1 merit.
-        merits = votes; 
-        return merits;
-    }
-
-    /**
-     * @dev Sends a message to another chain.
-     */
     function sendMessage(
-        bytes32 destinationChainID,
-        address destinationAddress,
-        address feeContractAddress,
-        uint256 feeAmount,
-        uint256 requiredGasLimit
-    ) external nonReentrant returns (uint256 messageID) {
-        string firstId = "De0vYJ7k7D";
-        string secondId = "qkGzdCRY9e";
-        string thirdId = "KM7HRQydNZ";
-        string fourthId = "eipbLjnyeG";
-        string fifthId = "gyGE5kaecT";
-
-        uint256 firstMerit = 10;
-        uint256 secondMerit = 5;
-        uint256 thirdMerit = 2;
-        uint256 fourthMerit = 1;
-        uint256 fifthMerit = 1;
-
-        bytes message = abi.encode(            
-            firstId,
-            firstMerit,
-            secondId,
-            secondMerit,
-            thirdId,
-            thirdMerit,
-            fourthId,
-            fourthMerit,
-            fifthId,
-            fifthMerit
+        address destinationAddress
+    ) external {
+        
+        bytes memory message = abi.encode(
+            0xF0f06058ca7B6e46E2B238F6d34A604DB1E2612f,
+            10,
+            0x8C12Aab5FFbE1F95B890f60832002F3Bbc6FA4CF,
+            5
         );
 
-        return
-            teleporterMessenger.sendCrossChainMessage(
-                TeleporterMessageInput({
-                    destinationChainID: destinationChainID,
-                    destinationAddress: destinationAddress,
-                    feeInfo: TeleporterFeeInfo({
-                        feeTokenAddress: address(0),
-                        amount: 0
-                    }),
-                    requiredGasLimit: requiredGasLimit,
-                    allowedRelayerAddresses: new address[](0),
-                    message: abi.encode(message)
-                })
-            );
+        teleporterMessenger.sendCrossChainMessage(
+            TeleporterMessageInput({
+                destinationBlockchainID: 0x9f3be606497285d0ffbb5ac9ba24aa60346a9b1812479ed66cb329f394a4b1c7,
+                destinationAddress: destinationAddress,
+                feeInfo: TeleporterFeeInfo({
+                    feeTokenAddress: address(0),
+                    amount: 0
+                }),
+                requiredGasLimit: 100000,
+                allowedRelayerAddresses: new address[](0),
+                message: message
+            })
+        );
     }
 }
+
+
+// pragma solidity 0.8.18;
+
+// import "https://github.com/ava-labs/teleporter/blob/main/contracts/src/Teleporter/ITeleporterMessenger.sol";
+
+// contract Vote {
+
+//     // // Mapping of imageId to votes received
+//     // mapping(bytes32 => uint256) public userVotes;
+
+//     // // Mapping of imageId to the publicId of the photographer (imgOwner)
+//     // mapping(bytes32 => bytes32) public imageOwners;
+
+//     // // Mapping of imgOwner to their total merits
+//     // mapping(bytes32 => uint256) public ownerMerits;
+
+//     // constructor() {
+//     //     // Initialize userVotes mapping within the constructor
+//     //     userVotes[bytes32("Image1")] = 91;
+//     //     userVotes[bytes32("Image2")] = 72;
+//     //     userVotes[bytes32("Image3")] = 77;
+//     //     userVotes[bytes32("Image4")] = 59;
+//     //     userVotes[bytes32("Image5")] = 36;
+        
+
+//     //     // Initialize imageOwners mapping
+//     //     imageOwners[bytes32("Image1")] = bytes32("De0vYJ7k7D");
+//     //     imageOwners[bytes32("Image2")] = bytes32("qkGzdCRY9e");
+//     //     imageOwners[bytes32("Image3")] = bytes32("KM7HRQydNZ");
+//     //     imageOwners[bytes32("Image4")] = bytes32("eipbLjnyeG");
+//     //     imageOwners[bytes32("Image5")] = bytes32("gyGE5kaecT");
+
+
+//     //     // Initialize ownerMerits mapping
+//     //     ownerMerits[bytes32("De0vYJ7k7D")] = 10;
+//     //     ownerMerits[bytes32("qkGzdCRY9e")] = 5;
+//     //     ownerMerits[bytes32("KM7HRQydNZ")] = 3;
+//     //     ownerMerits[bytes32("eipbLjnyeG")] = 1;
+//     //     ownerMerits[bytes32("gyGE5kaecT")] = 1;
+//     // }
+
+//     ITeleporterMessenger public immutable teleporterMessenger = ITeleporterMessenger(0x253b2784c75e510dD0fF1da844684a1aC0aa5fcf);
+//     function sendMessage(
+//         address destinationAddress
+//     ) external {
+//         address awardee1 = 0x128b51D4b6CaCF2D2825B1a5772D619b44300feb;
+//         uint voteAward1 = 10;
+//         address awardee2 = 0x8C12Aab5FFbE1F95B890f60832002F3Bbc6FA4CF;
+//         uint voteAward2 = 5;
+//         address awardee3 = 0x45a31B15d3CE9a6fb4dbF83A5FB7b7B8078c8888;
+//         uint voteAward3 = 3;
+//         address awardee4 = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+//         uint voteAward4 = 1;
+//         address awardee5 = 0x3EAb47F3e113BF2F6bc84A6080A7cC7c2c2B2C75;
+//         uint voteAward5 = 1;
+//         bytes memory message = abi.encode(
+//             awardee1,
+//             voteAward1,
+//             awardee2,
+//             voteAward2,
+//             awardee3,
+//             voteAward3,
+//             awardee4,
+//             voteAward4,
+//             awardee5,
+//             voteAward5
+//         );
+//         teleporterMessenger.sendCrossChainMessage(
+//             TeleporterMessageInput({
+//                 destinationBlockchainID: 0x9f3be606497285d0ffbb5ac9ba24aa60346a9b1812479ed66cb329f394a4b1c7,
+//                 destinationAddress: destinationAddress,
+//                 feeInfo: TeleporterFeeInfo({
+//                     feeTokenAddress: address(0),
+//                     amount: 0
+//                 }),
+//                 requiredGasLimit: 100000,
+//                 allowedRelayerAddresses: new address[](0),
+//                 message: message
+//             })
+//         );
+//     }
+// }
