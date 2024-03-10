5ab21aabf6b73e99477b703e23a7409e7104570f
yunokiisshin <yunokiisshin@gmail.com>
2024-03-10 15:25:18 +0000

    deployable contract created

--- smart-contracts/photograph.sol
+++ smart-contracts/photograph.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts@5.0.0/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@5.0.0/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts@5.0.0/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts@5.0.0/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts@4.4.1/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.4.1/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.4.1/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts@4.4.1/access/Ownable.sol";
+import "@openzeppelin/contracts@4.4.1/utils/Counters.sol";
+
 
 contract Photograph is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
-    constructor(address initialOwner) ERC721("AVALENS", "AVLS") Ownable(initialOwner) {}
+    constructor() ERC721("AVALENS", "AVLS") {}
 
     function safeMint(address to, string memory uri) public onlyOwner {
         _tokenIdCounter.increment();
@@ -26,10 +27,12 @@ contract Photograph is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
         return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
+
+    // Override required by Solidity for the combination of ERC721 and ERC721URIStorage
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
 }
-
-
-// reference: https://www.quicknode.com/guides/ethereum-development/nfts/how-to-create-and-deploy-an-erc-721-nft
