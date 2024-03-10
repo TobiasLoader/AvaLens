59cce77589dc9b0f8f2d8ea48afb9b3de5f3ee3c
yunokiisshin <yunokiisshin@gmail.com>
2024-03-10 00:04:00 +0000

    draft done

--- smart-contracts/photograph.sol
+++ smart-contracts/photograph.sol
@@ -1,12 +1,3 @@
-// IPFS should be established separately, and data like this:
-// {
-//   "name": "My image NFT",
-//   "description": "This is an NFT minted using my smart contract.",
-//   "image": "ipfs://<your-ipfs-hash-here>"
-// }
-
-// from https://www.quicknode.com/guides/ethereum-development/nfts/how-to-create-and-deploy-an-erc-721-nft
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
@@ -14,39 +5,31 @@ import "@openzeppelin/contracts@5.0.0/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@5.0.0/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts@5.0.0/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts@5.0.0/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Photograph is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
-    constructor(address initialOwner)
-        ERC721("MERIT", "MERIT")
-        Ownable(initialOwner)
-    {}
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
 
-    function safeMint(address to, uint256 tokenId, string memory uri)
-        public
-        onlyOwner
-    {
+    constructor(address initialOwner) ERC721("AVALENS", "AVLS") Ownable(initialOwner) {}
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (bool)
-    {
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 }
 
+
+// reference: https://www.quicknode.com/guides/ethereum-development/nfts/how-to-create-and-deploy-an-erc-721-nft
